630484871952adce8d00fb12b5d0ed1a542c9bc2
Vivek Gubba <vivekgubba59@gmail.com>
2026-09-01 10:28:36 +0530

    new Updated

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log("Hello, World!");
+// Regular function
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+// Arrow function
+const add = (a, b) => a + b;
+
+// Function with default parameter
+const introduce = (name = "Guest") => {
+  console.log(`Hi, I'm ${name}`);
+};
